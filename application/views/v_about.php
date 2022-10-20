360fa73591529771959eff9e20c4667cea62e401
nendaaffriasari <nendaaffriasari.gmail.com>
2022-10-20 16:34:35 +0700

    first commit

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : nenda</li>
+        <li>Nama Belakang : affriasari</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> yogyakarta</li>
+    </ul>
+ 
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+    <li>yogyakarta</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>tidak ada</li>
+        <li>tidak ada</li>
+     </ul>
+</section>
